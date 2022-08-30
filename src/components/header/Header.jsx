628fb68d8dcc023238ab3f8759aa003f4e43a1da
priyanshu <priyanshu6781@gmail.com>
2022-08-30 13:38:43 +0530

    header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Header>
+      <div className="container header__container">
+        <h5>Hello I am</h5>
+        <h1>Naruto</h1>
+        <h5 className="text-light">Full stack developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className='me'>
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </Header>
   )
 }
 
